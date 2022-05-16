147fb9cf06f961ded55125b5d53276d6ee6b6b72
Lorene Pecci <lorenepecci@gmail.com>
2022-05-16 17:18:16 -0300

    resolvendo conflitos

--- index.js
+++ index.js
@@ -30,6 +30,11 @@ app.get('/talker/:id', async (req, res) => {
   }
   return res.status(200).json(findID);
 });
+
+/* app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  res.status(200).json({token: })
+}); */
  
 app.listen(PORT, () => {
   console.log('Online');
